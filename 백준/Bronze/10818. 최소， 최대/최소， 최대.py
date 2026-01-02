bb80dc757a8a9751394cff200bdf162338f53a8a
finstats99 <finstats99@gmail.com>
2026-01-02 15:04:35 +0900

    [Bronze III] Title: 최소, 최대, Time: 312 ms, Memory: 132864 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,12 +1,9 @@
-N = int(input())
+import sys 
 
-my_list = input().split()
-int_list = []
+N = int(sys.stdin.readline())
 
-for i in range(len(my_list)) :
-    int_list.append(int(my_list[i]))
-
-max_val = max(int_list)
-min_val = min(int_list)
+my_list = list(map(int, sys.stdin.readline().split()))
+max_val = max(my_list)
+min_val = min(my_list)
 
 print(min_val, max_val)

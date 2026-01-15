8596541ae85cc6061b06129cf13bd305d18f4f54
finstats99 <finstats99@gmail.com>
2026-01-15 12:41:31 +0900

    [level 1] Title: 소수 찾기, Time: 3103.38 ms, Memory: 9.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -0,0 +1,15 @@
+def solution(n):
+    cnt = 0
+    
+    def is_prime(x):
+        for i in range(2, int(x**0.5) + 1):
+            if x%i == 0:
+                return False
+        return True
+    
+    
+    for i in range(2,n+1):
+        if is_prime(i):
+            cnt += 1
+            
+    return cnt

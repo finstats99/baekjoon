80c0c36754540d413bc411b3ffd1c4eed55d2913
finstats99 <finstats99@gmail.com>
2026-02-02 14:46:23 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 47.92 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,19 +1,24 @@
 def solution(participant, completion):
     part_dict = {}
     for i in participant:
-        if i in part_dict:
-            part_dict[i] += 1
-        else:
-            part_dict[i] = 1
+        part_dict[i] = 1 + part_dict.get(i, 0)
         
     for j in completion:
         if j in part_dict:
             part_dict[j] -= 1
-        else:
-            pass
     
     for key, val in part_dict.items():
         if val >= 1:
             return key
         else:
             pass
+        
+        
+        
+        
+def another(participant, completion):
+    from collections import Counter
+    
+    answer = Counter(participant) - Counter(completion)
+    return list(answer.keys())[0]
+    

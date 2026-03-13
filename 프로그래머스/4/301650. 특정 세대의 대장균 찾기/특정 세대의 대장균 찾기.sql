0e9fd17ec720908f885f0d3f5b1dc70606934cf4
finstats99 <finstats99@gmail.com>
2026-03-13 11:07:59 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,8 +1,12 @@
-with cond as (select a.id, b.parent_id, b.id as sibling_id
-from ecoli_data a, ecoli_data b
-where b.parent_id = a.id and a.parent_id is null)  # sibling_id : 2세대 부모역할
+select e3.id
+from (select e2.id  # 2세대 id를 출력으로)
+        from ecoli_data e1 # e1.id : 1세대 id
+        join ecoli_data e2 on e1.id = e2.parent_id
+        where e1.parent_id is null) e2
+        # 2세대 id와 3세대 parent_id연결
+join ecoli_data e3 on e2.id = e3.parent_id
 
-select c.id
-from ecoli_data c, cond
-where c.parent_id = cond.sibling_id # c.id : 3세대 (2세대의 자식)
-order by c.id
+# select e1.id, e2.id  # 2세대 id를 출력으로)
+# from ecoli_data e1 # e1.id : 1세대 id
+# join ecoli_data e2 on e1.id = e2.parent_id
+# where e1.parent_id is null
